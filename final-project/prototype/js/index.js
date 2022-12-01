968b0e4afa207102f0bc9aba8dfcdbd1ca322c3a
PercyNguyen7 <percy.nguyen7@gmail.com>
2022-12-01 17:46:31 -0500

    update trial 2

---
+++ final-project/prototype/js/index.js
@@ -0,0 +1,27 @@
+let posts1 = document.getElementsByClassName("posts-trial-1");
+let posts2 = document.getElementsByClassName("posts-trial-2");
+let posts3 = document.getElementsByClassName("posts-trial-3");
+
+
+let trial1Button = document.getElementById('trial-1-button');
+let trial2Button = document.getElementById('trial-2-button');
+let trial3Button = document.getElementById('trial-3-button');
+
+trial1Button.addEventListener("click", ()=>{
+    console.log('Displaying 1');
+    for (let i = 0; i < posts1.length; i++) {
+        posts1[i].setAttribute("data-visible",true);
+        posts2[i].setAttribute("data-visible",false);
+        // posts3[i].setAttribute("data-visible",false);
+      };
+});
+
+trial2Button.addEventListener("click", ()=>{
+    console.log('Displaying Trial 2');
+    for (let i = 0; i < posts1.length; i++) {
+        posts1[i].setAttribute("data-visible",false);
+        posts2[i].setAttribute("data-visible",true);
+        // posts3[i].setAttribute("data-visible",false);
+      };
+});
+
